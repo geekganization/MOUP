7c010f24a1540e757e02764d9747b127e4cebfbd
tls427wodnr <tlswodnr427@naver.com>
2025-06-13 20:09:51 +0900

    chore: #15 - merge용 수정

--- Routory/Routory/Presentation/MyPage/View/EditModal.swift
+++ Routory/Routory/Presentation/MyPage/View/EditModal.swift
@@ -132,10 +132,10 @@ private extension EditModal {
     }
 }
 
-extension UITextField {
-    func setLeftPadding(_ amount: CGFloat) {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.height))
-        self.leftView = paddingView
-        self.leftViewMode = .always
-    }
-}
+//extension UITextField {
+//    func setLeftPadding(_ amount: CGFloat) {
+//        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.height))
+//        self.leftView = paddingView
+//        self.leftViewMode = .always
+//    }
+//}

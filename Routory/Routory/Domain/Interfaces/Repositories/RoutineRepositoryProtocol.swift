3dd04e1780590d8bb7db6e0fa5cf59b4710add0f
_GGDol <bohg6799@naver.com>
2025-06-19 10:52:35 +0900

    feat: 오늘의 루틴 불러오는 로직 구현
    - 사용자 uid 전달 시 오늘의 루틴 불러오기
    
    Github issue #79

--- Routory/Routory/Domain/Interfaces/Repositories/RoutineRepositoryProtocol.swift
+++ Routory/Routory/Domain/Interfaces/Repositories/RoutineRepositoryProtocol.swift
@@ -5,10 +5,12 @@
 //  Created by 양원식 on 6/13/25.
 //
 import RxSwift
+import Foundation
 
 protocol RoutineRepositoryProtocol {
     func fetchAllRoutines(uid: String) -> Observable<[RoutineInfo]>
     func createRoutine(uid: String, routine: Routine) -> Observable<Void>
     func deleteRoutine(uid: String, routineId: String) -> Observable<Void>
-    func updateRoutine(uid: String, routineId: String, routine: Routine) -> Observable<Void> 
+    func updateRoutine(uid: String, routineId: String, routine: Routine) -> Observable<Void>
+    func fetchTodayRoutineEventsGroupedByWorkplace(uid: String, date: Date) -> Observable<[String: [CalendarEvent]]>
 }

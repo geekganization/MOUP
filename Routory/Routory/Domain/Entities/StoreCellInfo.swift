e1ad81a95a2ddb5195479d18a57c1dbc910e30aa
SongKyuSeob <sks5075@naver.com>
2025-06-26 19:36:59 +0900

    feat: 근무지, 매장 셀에 들어갈 정보 수정

--- Routory/Routory/Domain/Entities/StoreCellInfo.swift
+++ Routory/Routory/Domain/Entities/StoreCellInfo.swift
@@ -12,6 +12,12 @@ struct StoreCellInfo {
     let id: String
     let isOfficial: Bool
 
+    // 근무지 수정에 필요한 데이터
+    let category: String
+    let labelTitle: String
+    let showDot: Bool
+    let dotColor: String // 데이터 주입 시 String값을 UIColor로 바꾸는 작업 필요
+
     // 상단 고정 요소
     let storeName: String
     let daysUntilPayday: Int
